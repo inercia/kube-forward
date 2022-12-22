5aff8edf101115224de4706e2f158f546aa97225
Alvaro Saurin <alvaro.saurin@gmail.com>
2022-12-22 18:13:33 +0100

    Support multiple ports forwardings
    Signed-off-by: Alvaro Saurin <alvaro.saurin@gmail.com>

--- pkg/portforward/kube.go
+++ pkg/portforward/kube.go
@@ -17,50 +17,88 @@ import (
 	"k8s.io/client-go/transport/spdy"
 )
 
+type PortForwardPair struct {
+	LocalPort  int
+	RemotePort int
+}
+
 // KubeForwarder is a portforwarder for forwarding from a local port to a kubernetes Pod and port.
 // It is equivalent to "kubectl port-forward".
 type KubeForwarderConfig struct {
 	PodName      string
 	PodNamespace string
 
-	LocalPort  int
-	RemotePort int
+	// List of port forward pairs
+	Ports []PortForwardPair
 
 	RESTConfig *rest.Config
 	ClientSet  *kubernetes.Clientset
+}
+
+type KubeForwarderChannels struct {
+	ready chan struct{}
+	done  chan struct{}
+	stop  chan struct{}
+}
+
+func NewKubeForwarderChannels() *KubeForwarderChannels {
+	return &KubeForwarderChannels{
+		ready: make(chan struct{}),    // Closed when portforwarding ready.
+		done:  make(chan struct{}),    // Closed when portforwarding is done.
+		stop:  make(chan struct{}, 1), // is never closed by k8sportforward
+	}
 }
 
 type KubeForwarder struct {
 	sync.Mutex
 
 	KubeForwarderConfig
-	readyCh     chan struct{}
-	doneCh      chan struct{}
-	shouldStop  bool
-	stopCh      chan struct{}
-	stopChClose sync.Once
+	shouldStop bool
+
+	// channels per local port
+	channels map[int]*KubeForwarderChannels
+
+	stopChOnce sync.Once
 }
 
+// NewKubeForwarder creates a new KubeForwarder.
 func NewKubeForwarder(cfg KubeForwarderConfig) (*KubeForwarder, error) {
 	var err error
-	if cfg.LocalPort == 0 {
-		cfg.LocalPort, err = freeport.GetFreePort()
-		if err != nil {
-			return nil, err
+
+	// check all the ports are valid, looking for a free local port if not specified
+	ports := []PortForwardPair{}
+	for _, pair := range cfg.Ports {
+		if pair.LocalPort == 0 {
+			pair.LocalPort, err = freeport.GetFreePort()
+			if err != nil {
+				return nil, err
+			}
+		}
+		if pair.RemotePort > 0 {
+			// skip pairs with an invalid remote port
+			ports = append(ports, pair)
 		}
 	}
+	cfg.Ports = ports
 
-	return &KubeForwarder{
-		KubeForwarderConfig: cfg,
-		readyCh:             make(chan struct{}),    // Closed when portforwarding ready.
-		doneCh:              make(chan struct{}),    // Closed when portforwarding is done.
-		stopCh:              make(chan struct{}, 1), // is never closed by k8sportforward
-	}, nil
+	res := &KubeForwarder{KubeForwarderConfig: cfg}
+
+	res.channels = make(map[int]*KubeForwarderChannels, len(res.Ports))
+
+	for _, pair := range res.Ports {
+		localPort := pair.LocalPort
+		res.channels[localPort] = NewKubeForwarderChannels()
+	}
+
+	return res, nil
 }
 
-func (o *KubeForwarder) Run(ctx context.Context) (chan struct{}, error) {
-	go func() error {
-		log.Printf("Starting port-forward from :%d --> %s/%s:%d: dialing...", o.LocalPort, o.PodNamespace, o.PodName, o.RemotePort)
+// Run runs the port forwarding machinery.
+// The function returns immediately, and the port forwarding is done in goroutines. You should check if
+// the port-forwarding is ready with the Ready() channel.
+func (o *KubeForwarder) Run(ctx context.Context) (<-chan struct{}, error) {
+	pairWorker := func(pair PortForwardPair, channels *KubeForwarderChannels) error {
+		log.Printf("Starting port-forward from :%d --> %s/%s:%d: dialing...", pair.LocalPort, o.PodNamespace, o.PodName, pair.RemotePort)
 		req := o.ClientSet.CoreV1().RESTClient().Post().
 			Resource("pods").
 			Namespace(o.PodNamespace).
@@ -77,7 +115,7 @@ func (o *KubeForwarder) Run(ctx context.Context) (chan struct{}, error) {
 			http.MethodPost,
 			req.URL())
 
-		pfwdPorts := []string{fmt.Sprintf("%d:%d", o.LocalPort, o.RemotePort)}
+		pfwdPorts := []string{fmt.Sprintf("%d:%d", pair.LocalPort, pair.RemotePort)}
 
 		streams := genericclioptions.IOStreams{
 			In:     os.Stdin,
@@ -96,71 +134,97 @@ func (o *KubeForwarder) Run(ctx context.Context) (chan struct{}, error) {
 		for {
 			select {
 			case <-time.After(500 * time.Millisecond):
-				pfwd, err := k8sportforward.New(dialer, pfwdPorts, o.stopCh, o.readyCh, streams.Out, streams.ErrOut)
+				pfwd, err := k8sportforward.New(dialer, pfwdPorts, channels.stop, channels.ready, streams.Out, streams.ErrOut)
 				if err != nil {
-					log.Printf("error port-forwarding from :%d --> %d: %v", o.LocalPort, o.RemotePort, err)
+					log.Printf("error port-forwarding from :%d --> %d: %v", pair.LocalPort, pair.RemotePort, err)
 					continue
 				}
 
-				log.Printf("Running port-forward from :%d --> %s/%s:%d in a goroutine...", o.LocalPort, o.PodNamespace, o.PodName, o.RemotePort)
+				log.Printf("Running port-forward from :%d --> %s/%s:%d in a goroutine...", pair.LocalPort, o.PodNamespace, o.PodName, pair.RemotePort)
 				err = pfwd.ForwardPorts() // blocks
 				if err != nil {
-					log.Printf("error port-forwarding from :%d --> %d: %v", o.LocalPort, o.RemotePort, err)
+					log.Printf("error port-forwarding from :%d --> %d: %v", pair.LocalPort, pair.RemotePort, err)
 					continue
 				}
-
-				o.Lock()
-				shouldStop := o.shouldStop
-				o.Unlock()
 
 				// check if we are quitting because someone called Stop() or because the port-forward was broken
 				// in the last case, loop again
+				o.Lock()
+				shouldStop := o.shouldStop
+				o.Unlock()
 				if shouldStop {
-					log.Printf("Port-forward from :%d --> %s/%s:%d is done.", o.LocalPort, o.PodNamespace, o.PodName, o.RemotePort)
+					log.Printf("Port-forward from :%d --> %s/%s:%d is done.", pair.LocalPort, o.PodNamespace, o.PodName, pair.RemotePort)
 					break loop
 				}
-				log.Printf("Port-forward from :%d --> %s/%s:%d interrupted: retrying...", o.LocalPort, o.PodNamespace, o.PodName, o.RemotePort)
-				o.readyCh = make(chan struct{})
-				o.stopCh = make(chan struct{}, 1)
+				log.Printf("Port-forward from :%d --> %s/%s:%d interrupted: retrying...", pair.LocalPort, o.PodNamespace, o.PodName, pair.RemotePort)
+				channels.ready = make(chan struct{})
+				channels.stop = make(chan struct{}, 1)
 
 			case <-ctx.Done():
 				break loop
 			}
 		}
 
-		close(o.doneCh)
+		log.Printf("Port-forward is done...")
+		close(channels.done)
+
 		return nil
-	}()
+	}
+
+	// start a goroutine for each port-forward pair, NOT waiting for them to finish
+	for _, pair := range o.Ports {
+		go func(pair PortForwardPair) {
+			if err := pairWorker(pair, o.channels[pair.LocalPort]); err != nil {
+				log.Printf("error port-forwarding from :%d --> %d: %v", pair.LocalPort, pair.RemotePort, err)
+			}
+		}(pair)
+	}
 
 	// start a goroutine to wait for the cancellation of the context
 	go func() {
 		<-ctx.Done()
-		log.Printf("Context cancelled: stopping port-forward :%d --> %s/%s:%d.",
-			o.LocalPort, o.PodNamespace, o.PodName, o.RemotePort)
+		log.Printf("Context cancelled: stopping port-forwards to %s/%s.", o.PodNamespace, o.PodName)
 		o.Stop()
 	}()
 
-	return o.readyCh, nil
+	return o.Ready(), nil
 }
 
 func (o *KubeForwarder) Done() <-chan struct{} {
-	return o.doneCh
+	done := make(chan struct{})
+	go func() {
+		for _, v := range o.channels {
+			<-v.done
+		}
+		close(done)
+	}()
+	return done
 }
 
 func (o *KubeForwarder) Ready() <-chan struct{} {
-	return o.readyCh
+	ready := make(chan struct{})
+	go func() {
+		for _, v := range o.channels {
+			<-v.ready
+		}
+		close(ready)
+	}()
+
+	return ready
 }
 
 func (o *KubeForwarder) Stop() error {
 	// Make sure we only close the stopCh once.
-	o.stopChClose.Do(func() {
-		log.Printf("Stopping port-forward from :%d --> %s/%s:%d.", o.LocalPort, o.PodNamespace, o.PodName, o.RemotePort)
+	o.stopChOnce.Do(func() {
+		log.Printf("Stopping port-forwards to %s/%s.", o.PodNamespace, o.PodName)
 
 		o.Lock()
 		o.shouldStop = true
 		o.Unlock()
 
-		close(o.stopCh)
+		for _, v := range o.channels {
+			close(v.stop)
+		}
 	})
 	return nil
 }
